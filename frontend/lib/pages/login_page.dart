ac857ededfeb72ad77394e8278dd16737b8ac35d
jetasunanda <sunandajeta@gmail.com>
2026-03-08 16:15:27 +0700

    feat: enhance LoginPage UI with structured layout and form fields

--- frontend/lib/pages/login_page.dart
+++ frontend/lib/pages/login_page.dart
@@ -10,6 +10,41 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Genshin Import")));
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Text('Halaman Login', style: TextStyle(fontSize: 20)),
+            Text('Login/Register', style: TextStyle(fontSize: 14)),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Student Name",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
