8967c789f14d4a22d12c60549253dd0742fada42
jetasunanda <sunandajeta@gmail.com>
2026-03-08 22:45:17 +0700

    feat: introduce `AppColors` class with `gmailColor` constant.

--- frontend/lib/theme/app_colors.dart
+++ frontend/lib/theme/app_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-//   static const Color primary = Color(0xFF1E88E5);
-//   static const Color secondary = Color(0xFFFFC107);
-//   static const Color textDark = Color(0xFF333333);
+  //   static const Color primary = Color(0xFF1E88E5);
+  //   static const Color secondary = Color(0xFFFFC107);
+  //   static const Color textDark = Color(0xFF333333);
   static const Color gmailColor = Color(0xFF4285F4);
 }
