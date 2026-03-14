bf43b772ae02c1c5fe6e2888f05206fcefd58b1c
jetasunanda <sunandajeta@gmail.com>
2026-03-14 14:44:12 +0700

    feat: create LoginEmailPage with initial stateful widget structure

---
+++ frontend/lib/pages/login_email.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginEmailPage extends StatefulWidget {
+  const LoginEmailPage({super.key});
+
+  @override
+  State<LoginEmailPage> createState() => _LoginEmailPageState();
+}
+
+class _LoginEmailPageState extends State<LoginEmailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
