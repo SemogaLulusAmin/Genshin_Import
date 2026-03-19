34d2a0c79cc271334d250ac2b1398e50b7f6b1fa
jetasunanda <sunandajeta@gmail.com>
2026-03-20 05:57:29 +0700

    feat: enhance LoginEmailPage with logo and improved styling for text fields and buttons

--- frontend/lib/pages/login_email.dart
+++ frontend/lib/pages/login_email.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class LoginEmailPage extends StatefulWidget {
   const LoginEmailPage({super.key});
@@ -17,17 +18,22 @@ class _LoginEmailPageState extends State<LoginEmailPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Text(
-              "Genshin Import",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            const Text(
-              "Login",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+            SvgPicture.asset(
+              "assets/icons/logo_genshin_import.svg",
+              width: 80,
+              height: 80,
             ),
             const SizedBox(height: 24),
+            const Text(
+              "Login",
+              style: TextStyle(
+                fontFamily: 'Hywenhei',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 60),
             Form(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -35,6 +41,10 @@ class _LoginEmailPageState extends State<LoginEmailPage> {
                   TextFormField(
                     decoration: const InputDecoration(
                       labelText: "Email",
+                      labelStyle: TextStyle(
+                        fontFamily: 'Hywenhei',
+                        fontWeight: FontWeight.normal,
+                      ),
                       border: OutlineInputBorder(),
                     ),
                   ),
@@ -43,25 +53,115 @@ class _LoginEmailPageState extends State<LoginEmailPage> {
                     obscureText: true,
                     decoration: const InputDecoration(
                       labelText: "Password",
+                      labelStyle: TextStyle(fontFamily: 'Hywenhei'),
                       border: OutlineInputBorder(),
                     ),
                   ),
-                  const SizedBox(height: 24),
+                  const SizedBox(height: 32),
                   ElevatedButton(
                     onPressed: () {},
                     style: ElevatedButton.styleFrom(
                       padding: const EdgeInsets.symmetric(vertical: 16),
                     ),
-                    child: const Text("Login"),
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                        fontFamily: 'Hywenhei',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                        color: Colors.blue,
+                      ),
+                    ),
                   ),
-                  const SizedBox(height: 16),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text("Don't have an account? Register here"),
+                  const SizedBox(height: 40),
+                  Row(
+                    children: [
+                      const Text(
+                        "Don't have an account?",
+                        style: TextStyle(
+                          fontFamily: 'Hywenhei',
+                          fontSize: 12.0,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      TextButton(
+                        style:
+                            TextButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 8.0,
+                              ),
+                            ).copyWith(
+                              overlayColor: WidgetStateProperty.all(
+                                Colors.transparent,
+                              ),
+                            ),
+                        onPressed: () {},
+                        child: Container(
+                          padding: const EdgeInsets.only(bottom: 2),
+                          decoration: const BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.blue,
+                                width: 2.0,
+                              ),
+                            ),
+                          ),
+                          child: const Text(
+                            "Register here",
+                            style: TextStyle(
+                              fontFamily: 'Hywenhei',
+                              fontSize: 12.0,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text("Forgot your password? Change Password"),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      const Text(
+                        'Forgot your password?',
+                        style: TextStyle(
+                          fontFamily: 'Hywenhei',
+                          fontSize: 12.0,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      TextButton(
+                        style:
+                            TextButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 8.0,
+                              ),
+                            ).copyWith(
+                              overlayColor: WidgetStateProperty.all(
+                                Colors.transparent,
+                              ),
+                            ),
+                        onPressed: () {},
+                        child: Container(
+                          padding: const EdgeInsets.only(bottom: 2),
+                          decoration: const BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.blue,
+                                width: 2.0,
+                              ),
+                            ),
+                          ),
+                          child: const Text(
+                            "Change Password",
+                            style: TextStyle(
+                              fontFamily: 'Hywenhei',
+                              fontSize: 12.0,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
