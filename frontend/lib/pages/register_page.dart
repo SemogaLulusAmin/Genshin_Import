c341536ba60313c84da4c71893b0da0592eecf4c
jetasunanda <sunandajeta@gmail.com>
2026-03-20 19:43:59 +0700

    feat: add initial registration page UI with form fields and navigation to login.

--- frontend/lib/pages/register_page.dart
+++ frontend/lib/pages/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -10,6 +11,126 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SvgPicture.asset(
+              'assets/icons/logo_genshin_import.svg',
+              width: 80,
+              height: 80,
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              'Register',
+              style: TextStyle(
+                fontFamily: 'Hywenhei',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 60),
+            Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: "Email",
+                      labelStyle: TextStyle(fontFamily: 'Hywenhei'),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: "Your Password",
+                      labelStyle: TextStyle(fontFamily: 'Hywenhei'),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: "Confirm Password",
+                      labelStyle: TextStyle(fontFamily: 'Hywenhei'),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                    ),
+                    child: const Text(
+                      "Register",
+                      style: TextStyle(
+                        fontFamily: 'Hywenhei',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  Row(
+                    children: [
+                      const Text(
+                        "Do you have an account?",
+                        style: TextStyle(
+                          fontFamily: 'Hywenhei',
+                          fontSize: 12.0,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      TextButton(
+                        style:
+                            TextButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 8.0,
+                              ),
+                            ).copyWith(
+                              overlayColor: WidgetStateProperty.all(
+                                Colors.transparent,
+                              ),
+                            ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.only(bottom: 2),
+                          decoration: const BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.blue,
+                                width: 2.0,
+                              ),
+                            ),
+                          ),
+                          child: const Text(
+                            "Login here",
+                            style: TextStyle(
+                              fontFamily: 'Hywenhei',
+                              fontSize: 12.0,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
