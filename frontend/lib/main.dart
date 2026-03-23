e9ad60e6350445c4e722ecbb091f3f4ed217978b
jetasunanda <sunandajeta@gmail.com>
2026-03-24 00:50:38 +0700

    feat: initial setup of Genshin Import Flutter frontend application including core services, models, providers, and main app structure.

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/pages/test_page.dart';
 import 'pages/main_page.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Genshin Import',
-      home: const MainPage(),
+      home: const TestPage(),
     );
   }
 }
