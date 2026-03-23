0f7422f025f4cca561ecaac8dc863a58dbdd3f1b
jetasunanda <sunandajeta@gmail.com>
2026-03-24 00:04:27 +0700

    feat: implement initial registration page with form validation, password visibility toggles, and basic registration logic.

--- frontend/lib/pages/register_page.dart
+++ frontend/lib/pages/register_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -9,6 +11,55 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  bool _isLoading = false;
+  bool _isObscurePassword = true;
+  bool _isObscureConfirmPassword = true;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _register() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final url = Uri.parse('http://URL_BACKEND/api/login');
+
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'email': _emailController.text,
+          'password': _passwordController.text,
+        }),
+      );
+      print(response.body);
+    } catch (e) {
+      print(e);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,10 +85,22 @@ class _RegisterPageState extends State<RegisterPage> {
             ),
             const SizedBox(height: 60),
             Form(
+              key: _formKey,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  TextField(
+                  TextFormField(
+                    controller: _emailController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Email can't be empty";
+                      }
+                      final emailRegex = RegExp(r'^[\w-\.]+@[\w-\.]');
+                      if (!emailRegex.hasMatch(value)) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
                     decoration: const InputDecoration(
                       labelText: "Email",
                       labelStyle: TextStyle(fontFamily: 'Hywenhei'),
@@ -45,38 +108,88 @@ class _RegisterPageState extends State<RegisterPage> {
                     ),
                   ),
                   const SizedBox(height: 16),
-                  TextField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
+                  TextFormField(
+                    controller: _passwordController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Password can't be empty";
+                      }
+                      if (value.length < 8) {
+                        return "Password must be at least 8 characters";
+                      }
+                      return null;
+                    },
+                    obscureText: _isObscurePassword,
+                    decoration: InputDecoration(
                       labelText: "Your Password",
                       labelStyle: TextStyle(fontFamily: 'Hywenhei'),
                       border: OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isObscurePassword
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isObscurePassword = !_isObscurePassword;
+                          });
+                        },
+                      ),
                     ),
                   ),
                   const SizedBox(height: 16),
-                  TextField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
+                  TextFormField(
+                    controller: _confirmPasswordController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Password can't be empty";
+                      }
+                      if (value.length < 8) {
+                        return "Password must be at least 8 characters";
+                      }
+                      if (value != _passwordController.text) {
+                        return "Passwords do not match";
+                      }
+                      return null;
+                    },
+                    obscureText: _isObscureConfirmPassword,
+                    decoration: InputDecoration(
                       labelText: "Confirm Password",
                       labelStyle: TextStyle(fontFamily: 'Hywenhei'),
                       border: OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isObscureConfirmPassword
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isObscureConfirmPassword =
+                                !_isObscureConfirmPassword;
+                          });
+                        },
+                      ),
                     ),
                   ),
                   const SizedBox(height: 32),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: _isLoading ? null : _register,
                     style: ElevatedButton.styleFrom(
                       padding: const EdgeInsets.symmetric(vertical: 16),
                     ),
-                    child: const Text(
-                      "Register",
-                      style: TextStyle(
-                        fontFamily: 'Hywenhei',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
-                        color: Colors.blue,
-                      ),
-                    ),
+                    child: _isLoading
+                        ? const CircularProgressIndicator()
+                        : const Text(
+                            "Register",
+                            style: TextStyle(
+                              fontFamily: 'Hywenhei',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                              color: Colors.blue,
+                            ),
+                          ),
                   ),
                   const SizedBox(height: 40),
                   Row(
