3ca89cfd267abe7d1a7601f23cb3360c5d8088db
unknown <edwardariatanujaya@gmail.com>
2026-03-24 19:53:32 +0700

    feat: add buy for weapon for user

--- backend/src/routes/userWeaponRoutes.js
+++ backend/src/routes/userWeaponRoutes.js
@@ -1,11 +1,58 @@
 import express from 'express';
 import pool from '../db.js';
+import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/purchased-weapons/:userID',authenticateToken, async (req, res) => {
+router.post('/buy/:weaponID',authenticateToken, async (req,res) => {
+    const userID = req.user.id;
+
+    const {weaponID, quantity, price} = req.body;
+
+    const connection = await pool.getConnection();
+
     try {
-        const {userID} = req.params;
+
+        await connection.beginTransaction();
+
+        const [weapon] = await connection.execute(
+            "SELECT stock, price FROM Weapon WHERE weaponID = ? FOR UPDATE", 
+            [weaponID]
+        );
+
+        const [user] = await connection.execute(
+            "SELECT money FROM User WHERE userID = ? FOR UPDATE",
+            [userID]
+        )
+
+        if (weapon.length === 0) throw new Error("There's no such product!");
+        if (weapon[0].stock < quantity) throw new Error("Quantity over stock!");
+        const totalPrice = weapon[0].price * quantity;
+        if (totalPrice > user[0].money) throw new Error("Not enough money!");
+
+        await connection.execute("UPDATE User SET money = money - ? WHERE userID = ?", [totalPrice, userID]);
+
+        await connection.execute("UPDATE Weapon SET stock = stock - ? WHERE weaponID = ?", [quantity, weaponID])
+
+        await connection.execute("INSERT INTO WeaponTransaction (userID, weaponID, stock) VALUES (?, ?, ?)",[userID, weaponID, quantity]);
+
+        await connection.commit();
+
+        res.status(200);
+
+    } catch (error){
+        await connection.rollback();
+        console.log(error.message);
+        res.status(500);
+    } finally {
+        connection.release();
+    }
+
+})
+
+router.get('/purchased-weapons/',authenticateToken, async (req, res) => {
+    try {
+        const userID = req.user.id;
 
         if (!userID) return res.status(400).json({ error: 'User ID is required' });
 
@@ -26,9 +73,9 @@ router.get('/purchased-weapons/:userID',authenticateToken, async (req, res) => {
     }
 });
 
-router.get('not-purchased-weapons/:userID', authenticateToken, async (req, res) => {
+router.get('not-purchased-weapons/', authenticateToken, async (req, res) => {
     try {
-        const {userID} = req.params;
+        const userID = req.user.id;
 
         if (!userID) return res.status(400).json({ error: 'User ID is required' });
 
