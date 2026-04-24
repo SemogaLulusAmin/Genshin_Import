0608e2ddc339f08d3bcd4ac871c5c352f52bc7fd
jetasunanda <sunandajeta@gmail.com>
2026-04-24 11:31:17 +0700

    feat: update TestPage to use Scaffold for improved layout structure

--- frontend/lib/pages/test_page.dart
+++ frontend/lib/pages/test_page.dart
@@ -10,6 +10,6 @@ class TestPage extends StatefulWidget {
 class _TestPageState extends State<TestPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
