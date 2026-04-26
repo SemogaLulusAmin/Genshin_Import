01fc0e622780d65fb8db53ee314ea07f9ec6b4dc
jetasunanda <sunandajeta@gmail.com>
2026-04-26 18:57:01 +0700

    feat: update MainNavigationScreen to use GalleryShopScreen instead of placeholder text

--- frontend/lib/state/main_navigation_state.dart
+++ frontend/lib/state/main_navigation_state.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screens/home/gallery_shop_screen.dart';
 import 'package:frontend/widgets/main_navigation_bar.dart';
 
 class MainNavigationScreen extends StatefulWidget {
@@ -13,7 +14,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   // Placeholder untuk 5 halaman wajib sesuai dokumen
   final List<Widget> _pages = [
-    const Center(child: Text('Gallery & Shop Screen')),
+    const GalleryShopScreen(),
     const Center(child: Text('Inventory Screen')),
     const Center(child: Text('Delivery Package Screen')),
     const Center(child: Text('Profile Screen')),
