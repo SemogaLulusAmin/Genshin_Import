d669bcbb0981a88d05d43e521a867661240aea5e
jetasunanda <sunandajeta@gmail.com>
2026-04-26 18:56:48 +0700

    feat: add GalleryShopScreen with initial layout and welcome message

---
+++ frontend/lib/screens/home/gallery_shop_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../../core/app_colors.dart';
+
+class GalleryShopScreen extends StatefulWidget {
+  const GalleryShopScreen({super.key});
+
+  @override
+  State<GalleryShopScreen> createState() => _GalleryShopScreenState();
+}
+
+class _GalleryShopScreenState extends State<GalleryShopScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            color: AppColors.primary,
+            height: 60,
+            child: const Center(child: Text('Poin: ')),
+          ),
+          SingleChildScrollView(child: Text('Welcome to Genshin Import!')),
+        ],
+      ),
+    );
+  }
+}
