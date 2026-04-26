124d2d85175eb88fe6ed738e9e3c830c802f93fe
jetasunanda <sunandajeta@gmail.com>
2026-04-26 18:54:07 +0700

    feat: update main.dart to set MainNavigationScreen as home and import necessary screens

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screens/home/home_screen.dart';
+import 'package:frontend/state/main_navigation_state.dart';
 import 'core/app_theme.dart';
 import 'widgets/main_navigation_bar.dart';
 import 'screens/auth/login_screen.dart';
@@ -21,43 +23,9 @@ class GenshinImportApp extends StatelessWidget {
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.light,
 
-      home: const LoginScreen(),
-    );
-  }
-}
-
-class MainNavigationScreen extends StatefulWidget {
-  const MainNavigationScreen({super.key});
-
-  @override
-  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
-}
-
-class _MainNavigationScreenState extends State<MainNavigationScreen> {
-  int _selectedIndex = 0;
-
-  // Placeholder untuk 5 halaman wajib sesuai dokumen
-  final List<Widget> _pages = [
-    const Center(child: Text('Gallery & Shop Screen')),
-    const Center(child: Text('Inventory Screen')),
-    const Center(child: Text('Delivery Package Screen')),
-    const Center(child: Text('Profile Screen')),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // Menggunakan IndexedStack agar state halaman tidak hilang saat pindah tab
-      body: IndexedStack(index: _selectedIndex, children: _pages),
-      // Memanggil komponen Navbar terpisah
-      bottomNavigationBar: MainNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-      ),
+      // home: const LoginScreen(),
+      // home: const HomeScreen(),
+      home: MainNavigationScreen(),
     );
   }
 }
