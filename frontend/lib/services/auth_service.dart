b276eeec27612ba80f4179d46045d06999ab73e5
jetasunanda <sunandajeta@gmail.com>
2026-05-01 18:17:51 +0700

    feat: enhance login functionality with user data storage in SharedPreferences

--- frontend/lib/services/auth_service.dart
+++ frontend/lib/services/auth_service.dart
@@ -1,7 +1,13 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Authservice {
+  // final String _baseUrl = Platform.isAndroid
+  //     ? "http://10.0.2.2:3000/auth"
+  //     : "http://localhost:3000/auth";
+
   final String _baseUrl = "http://localhost:3000/auth";
 
   Future<Map<String, dynamic>> login(String email, String password) async {
@@ -13,6 +19,12 @@ class Authservice {
       );
 
       final data = json.decode(response.body);
+
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('jwt_token', data['token']);
+      await prefs.setString('username', data['user']['username']);
+      await prefs.setString('email', data['user']['money']);
+      await prefs.setString('email', data['user']['roles']);
 
       if (response.statusCode == 200) {
         return {"success": true, "token": data['token'], "user": data['user']};
