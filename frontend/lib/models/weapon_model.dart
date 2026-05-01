2159e172f33b6fab8d5cdbabc84df560e2d8ea88
jetasunanda <sunandajeta@gmail.com>
2026-05-01 11:59:31 +0700

    feat: add Weapon model with JSON serialization

---
+++ frontend/lib/models/weapon_model.dart
@@ -0,0 +1,43 @@
+class Weapon {
+  final String weaponID;
+  final String name;
+  final String type;
+  final String rarity;
+  final String baseAttack;
+  final String subStat;
+  final String passiveName;
+  final String passiveDesc;
+  final double price;
+  final int stock;
+  final String imageUrl;
+
+  Weapon({
+    required this.weaponID,
+    required this.name,
+    required this.type,
+    required this.rarity,
+    required this.baseAttack,
+    required this.subStat,
+    required this.passiveName,
+    required this.passiveDesc,
+    required this.price,
+    required this.stock,
+    required this.imageUrl,
+  });
+
+  factory Weapon.fromJson(Map<String, dynamic> json) {
+    return Weapon(
+      weaponID: json['weaponID'],
+      name: json['name'],
+      type: json['type'],
+      rarity: json['rarity'],
+      baseAttack: json['baseAttack'],
+      subStat: json['subStat'],
+      passiveName: json['passiveName'],
+      passiveDesc: json['passiveDesc'],
+      price: json['price'].toDouble(),
+      stock: json['stock'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+}
