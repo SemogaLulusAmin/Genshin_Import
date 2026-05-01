64807b513442292b63957e96c8a5522d1f742e26
jetasunanda <sunandajeta@gmail.com>
2026-05-01 11:59:41 +0700

    feat: add WeaponService to fetch weapon data from API

---
+++ frontend/lib/services/weapon_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/weapon_model.dart';
+
+class WeaponService {
+  // Jadikan base URL konstanta agar mudah diubah saat naik ke Production
+  static const String baseUrl = 'http://10.0.2.2:3000';
+  // static const String baseUrl = 'http://localhost:3000';
+
+  Future<List<Weapon>> getWeapons() async {
+    try {
+      final response = await http.get(Uri.parse('$baseUrl/weapon'));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonResponse = json.decode(response.body);
+        return jsonResponse.map((data) => Weapon.fromJson(data)).toList();
+      } else {
+        throw Exception('Failed to load weapons');
+      }
+    } catch (e) {
+      throw Exception('Network error: $e');
+    }
+  }
+}
