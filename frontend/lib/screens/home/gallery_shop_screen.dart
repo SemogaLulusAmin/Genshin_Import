0efe30b79350c48d713255bef6564a063846b048
jetasunanda <sunandajeta@gmail.com>
2026-05-01 11:59:36 +0700

    feat: implement weapon search functionality in GalleryShopScreen

--- frontend/lib/screens/home/gallery_shop_screen.dart
+++ frontend/lib/screens/home/gallery_shop_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import '../../core/app_colors.dart';
+import '../../models/weapon_model.dart';
+import '../../services/weapon_service.dart';
 
 class GalleryShopScreen extends StatefulWidget {
   const GalleryShopScreen({super.key});
@@ -9,6 +15,17 @@ class GalleryShopScreen extends StatefulWidget {
 }
 
 class _GalleryShopScreenState extends State<GalleryShopScreen> {
+  final WeaponService _weaponService = WeaponService();
+  late Future<List<Weapon>> _weaponFuture;
+
+  // Future<void> fetchDataArtifacts() async {}
+
+  @override
+  void initState() {
+    super.initState();
+    _weaponFuture = _weaponService.getWeapons();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,25 +33,42 @@ class _GalleryShopScreenState extends State<GalleryShopScreen> {
         child: Column(
           children: [
             Header(),
-            SingleChildScrollView(
-              child: Column(
-                children: List.generate(
-                  10,
-                  (index) => Container(
-                    height: 100,
-                    color: Colors.grey[300],
-                    margin: const EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 16,
-                    ),
-                    child: const Center(child: Text('Item')),
-                  ),
-                ),
-              ),
-            ),
+            SingleChildScrollView(child: Column(children: [SearchBarWdiget()])),
           ],
         ),
       ),
+    );
+  }
+}
+
+class SearchBarWdiget extends StatelessWidget {
+  const SearchBarWdiget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SearchAnchor(
+      builder: (BuildContext context, SearchController controller) {
+        return SearchBar(
+          controller: controller,
+          padding: WidgetStatePropertyAll<EdgeInsets>(
+            EdgeInsets.symmetric(horizontal: 16.0),
+          ),
+          leading: const Icon(Icons.search),
+          hintText: 'Search weapons and artifacts here!',
+          onTap: () {
+            controller.openView();
+          },
+          onChanged: (_) {
+            controller.openView();
+          },
+        );
+      },
+      suggestionsBuilder: (BuildContext context, SearchController controller) {
+        return [
+          ListTile(title: Text("Suggestion 1")),
+          ListTile(title: Text("Suggestion 2")),
+        ];
+      },
     );
   }
 }
