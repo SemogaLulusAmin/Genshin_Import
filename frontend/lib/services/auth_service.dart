fa1f7f78e252c7fa74ff8c118f70a069260695f4
jetasunanda <sunandajeta@gmail.com>
2026-05-01 20:46:22 +0700

    feat: update login and registration logic with improved error handling and user role storage

--- frontend/lib/services/auth_service.dart
+++ frontend/lib/services/auth_service.dart
@@ -20,28 +20,52 @@ class Authservice {
 
       final data = json.decode(response.body);
 
-      final SharedPreferences prefs = await SharedPreferences.getInstance();
-      await prefs.setString('jwt_token', data['token']);
-      await prefs.setString('username', data['user']['username']);
-      await prefs.setString('email', data['user']['money']);
-      await prefs.setString('email', data['user']['roles']);
-
       if (response.statusCode == 200) {
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString('jwt_token', data['token']);
+        await prefs.setString('username', data['user']['username']);
+        await prefs.setString('email', data['user']['money']);
+        await prefs.setString('roles', data['user']['roles']);
         return {"success": true, "token": data['token'], "user": data['user']};
       } else if (response.statusCode == 401) {
-        return {
-          "success": false,
-          "message": "message: ${data['message'] ?? "Wrong password"}",
-        };
+        return {"success": false, "message": "Wrong password"};
       } else if (response.statusCode == 404) {
-        return {
-          "success": false,
-          "message": "message: ${data['message'] ?? "User not found"}",
-        };
+        return {"success": false, "message": "User not found"};
       } else {
         return {
           "success": false,
           "message": "message: ${data['message'] ?? "Login Failed"}",
+        };
+      }
+    } catch (e) {
+      return {"success": false, "message": "message: $e"};
+    }
+  }
+
+  Future<Map<String, dynamic>> register(
+    String username,
+    String email,
+    String password,
+  ) async {
+    try {
+      final response = await http.post(
+        Uri.parse("$_baseUrl/register"),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({
+          "username": username,
+          "email": email,
+          "password": password,
+        }),
+      );
+
+      final data = json.decode(response.body);
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return {"success": true, "message": data['message']};
+      } else {
+        return {
+          "success": false,
+          "message": "message: ${data['message'] ?? "Registration Failed"}",
         };
       }
     } catch (e) {
