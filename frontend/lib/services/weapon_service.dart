7abd1d38cc45d24105343d45d49f5e30b3af5f38
jetasunanda <sunandajeta@gmail.com>
2026-05-01 12:42:54 +0700

    fix: update base URL in WeaponService for local development

--- frontend/lib/services/weapon_service.dart
+++ frontend/lib/services/weapon_service.dart
@@ -3,8 +3,8 @@ import 'package:http/http.dart' as http;
 import '../models/weapon_model.dart';
 
 class WeaponService {
-  // static const String baseUrl = 'http://10.0.2.2:3000';
-  static const String baseUrl = 'http://localhost:3000/';
+  static const String baseUrl = 'http://10.0.2.2:3000';
+  // static const String baseUrl = 'http://localhost:3000';
 
   Future<List<Weapon>> getWeapons() async {
     try {
