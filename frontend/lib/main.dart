1d32572bc8e8623048944a5612ff242e7634936c
jetasunanda <sunandajeta@gmail.com>
2026-05-01 21:36:43 +0700

    refactor: remove unused MainNavigationScreen class and related code

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/state/main_navigation_state.dart';
 import 'core/app_theme.dart';
-import 'widgets/main_navigation_bar.dart';
 import 'screens/auth/login_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -43,38 +42,4 @@ class GenshinImportApp extends StatelessWidget {
   }
 }
 
-class MainNavigationScreen extends StatefulWidget {
-  const MainNavigationScreen({super.key});
 
-  @override
-  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
-}
-
-class _MainNavigationScreenState extends State<MainNavigationScreen> {
-  int _selectedIndex = 0;
-
-  // Placeholder untuk 5 halaman wajib sesuai dokumen
-  final List<Widget> _pages = [
-    const Center(child: Text('Gallery & Shop Screen')),
-    const Center(child: Text('Inventory Screen')),
-    const Center(child: Text('Delivery Package Screen')),
-    const Center(child: Text('Profile Screen')),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // Menggunakan IndexedStack agar state halaman tidak hilang saat pindah tab
-      body: IndexedStack(index: _selectedIndex, children: _pages),
-      // Memanggil komponen Navbar terpisah
-      bottomNavigationBar: MainNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-      ),
-    );
-  }
-}
