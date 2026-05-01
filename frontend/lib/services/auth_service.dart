ce40fee334c40529d92af3b8423b74da91436083
jetasunanda <sunandajeta@gmail.com>
2026-05-01 16:33:55 +0700

    fix: update base URL for authentication and improve error message handling in login response

--- frontend/lib/services/auth_service.dart
+++ frontend/lib/services/auth_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Authservice {
-  final String _baseUrl = "http://localhost:3000";
+  final String _baseUrl = "http://localhost:3000/auth";
 
   Future<Map<String, dynamic>> login(String email, String password) async {
     try {
@@ -12,15 +12,30 @@ class Authservice {
         body: jsonEncode({"email": email, "password": password}),
       );
 
-      final data = jsonDecode(response.body);
+      print("Raw login response: ${response.body}");
+      final data = json.decode(response.body);
+      print("Login response: $data");
 
       if (response.statusCode == 200) {
         return {"success": true, "token": data['token'], "user": data['user']};
+      } else if (response.statusCode == 401) {
+        return {
+          "success": false,
+          "message": "message: ${data['message'] ?? "Wrong password"}",
+        };
+      } else if (response.statusCode == 404) {
+        return {
+          "success": false,
+          "message": "message: ${data['message'] ?? "User not found"}",
+        };
       } else {
-        return {"success": false, "message": data['message'] ?? "Login Failed"};
+        return {
+          "success": false,
+          "message": "message: ${data['message'] ?? "Login Failed"}",
+        };
       }
     } catch (e) {
-      return {"success": false, "message": "Failed to login: $e"};
+      return {"success": false, "message": "message: $e"};
     }
   }
 }
