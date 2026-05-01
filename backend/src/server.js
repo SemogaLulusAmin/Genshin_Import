aa4567ee020d51c65e431d732871066d9a558c4f
unknown <edwardariatanujaya@gmail.com>
2026-05-01 13:05:45 +0700

    fix: add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import pool from './db.js';
 import 'dotenv/config';
 import authRoutes from './routes/authRoutes.js';
@@ -11,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/assets', express.static('public/assets'));
 
 app.use('/auth', authRoutes);
