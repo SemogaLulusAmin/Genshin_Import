359f6f84db0680303df80f1760fab6b1589c9190
AyamBerenang <varel.lie29@gmail.com>
2026-05-04 16:26:45 +0700

    style: Refactor import statements and formatting in server.js for consistency

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import pool from './db.js';
-import 'dotenv/config';
-import authRoutes from './routes/authRoutes.js';
-import itemRoutes from './routes/weaponRoutes.js';
-import userweapon from './routes/userWeaponRoutes.js';
-import userartifact from './routes/userArtifactRoutes.js';
-import artifactRoutes from './routes/ArtifactRoutes.js';
-import cors from 'cors';
+import express from "express";
+
+import pool from "./db.js";
+import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js";
+import itemRoutes from "./routes/weaponRoutes.js";
+import userweapon from "./routes/userWeaponRoutes.js";
+import userartifact from "./routes/userArtifactRoutes.js";
+import artifactRoutes from "./routes/ArtifactRoutes.js";
+import cors from "cors";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json())
-app.use('/assets', express.static('public/assets'));
+app.use(express.json());
+app.use("/assets", express.static("public/assets"));
 
-app.use('/auth', authRoutes);
-app.use('/userWeapon', userweapon);
-app.use('/weapon', itemRoutes);
-app.use('/userArtifact', userartifact);
-app.use('/artifact',artifactRoutes);
+app.use("/auth", authRoutes);
+app.use("/userWeapon", userweapon);
+app.use("/weapon", itemRoutes);
+app.use("/userArtifact", userartifact);
+app.use("/artifact", artifactRoutes);
 app.listen(PORT, () => {
-    console.log(`Yeah server online on http://localhost:${PORT}`);
+  console.log(`Yeah server online on http://localhost:${PORT}`);
 });
-
