1622c9df6bb17b6b6f21aef667091ebeb3dd0b4f
jetasunanda <sunandajeta@gmail.com>
2026-05-07 11:15:34 +0700

    feat: create UserService class with platform-aware base URL configuration

--- frontend/lib/services/user_service.dart
+++ frontend/lib/services/user_service.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import '../models/user_model.dart';
 
-class Userservice {
+class UserService {
   String get _baseUrl {
     if (kIsWeb) {
       return "http://localhost:3000/users";
