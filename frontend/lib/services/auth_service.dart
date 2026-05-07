bcbc701f39ead4875845c2ee5481b6931c6484f7
jetasunanda <sunandajeta@gmail.com>
2026-05-07 11:15:29 +0700

    feat: implement auth service for login/registration and initialize user service scaffold

--- frontend/lib/services/auth_service.dart
+++ frontend/lib/services/auth_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/foundation.dart';
 
-class AuthService {
+class Authservice {
   String get _baseUrl {
     if (kIsWeb) {
       return "http://localhost:3000/auth";
