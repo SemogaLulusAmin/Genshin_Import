ac6d1a3a024fa12866250cd4c38f8a1d05b35c4e
jetasunanda <sunandajeta@gmail.com>
2026-05-08 17:57:13 +0700

    feat: implement user authentication services and add money display widget

--- frontend/lib/models/user_model.dart
+++ frontend/lib/models/user_model.dart
@@ -1,31 +1,8 @@
-// class User {
-//   final String id;
-//   final String username;
-//   final double money;
-//   final String roles;
-
-//   User({
-//     required this.id,
-//     required this.username,
-//     required this.money,
-//     required this.roles,
-//   });
-
-//   factory User.fromJson(Map<String, dynamic> json) {
-//     return User(
-//       id: json['id'],
-//       username: json['username'],
-//       money: json['money'].toDouble(),
-//       roles: json['roles'],
-//     );
-//   }
-// }
-
 class User {
   final String id;
   final String username;
   final String email; // ✅ tambahin ini
-  final double money;
+  final int money;
   final String roles;
 
   User({
@@ -40,8 +17,8 @@ class User {
     return User(
       id: json['id'] ?? '',
       username: json['username'] ?? '',
-      email: json['email'] ?? '', // ✅ ambil dari DB
-      money: (json['money'] ?? 0).toDouble(),
+      email: json['email'] ?? '',
+      money: (num.tryParse(json['money'].toString()) ?? 0).toInt(),
       roles: json['roles'] ?? '',
     );
   }
