c1822cc93ecdea5da6abb068ea843d52f80f600e
jetasunanda <sunandajeta@gmail.com>
2026-05-08 10:55:11 +0700

    feat: add ProfileState for data fetching and ArtifactCard widget for displaying artifact details

--- frontend/lib/states/profile_state.dart
+++ frontend/lib/states/profile_state.dart
@@ -1,42 +1,32 @@
+import 'package:flutter/foundation.dart';
 import 'package:frontend/models/user_model.dart';
 import 'package:frontend/services/user_service.dart';
 
-class ProfileState {
-  bool _isLoading;
-  User _user;
-  String _errorMessage;
-  final UserService userService = UserService();
+class ProfileState extends ChangeNotifier {
+  final UserService userService;
 
-  ProfileState({
-    required bool isLoading,
-    required User user,
-    required String errorMessage,
-  }) : _isLoading = isLoading,
-       _user = user,
-       _errorMessage = errorMessage;
+  bool _isLoading = false;
+  User? _user;
+  String? _errorMessage;
+
+  ProfileState({required this.userService});
 
   bool get isLoading => _isLoading;
-  User get user => _user;
-  String get errorMessage => _errorMessage;
+  User? get user => _user;
+  String? get errorMessage => _errorMessage;
 
-  ProfileState copyWith({bool? isLoading, User? user, String? errorMessage}) {
-    return ProfileState(
-      isLoading: isLoading ?? _isLoading,
-      user: user ?? _user,
-      errorMessage: errorMessage ?? _errorMessage,
-    );
-  }
-
-  loadProfileData(String token) async {
+  Future<void> loadProfileData() async {
     try {
       _isLoading = true;
-      // Wait userService can connect to database
-      // final response = await userService.fetchUserProfile(token);
-      // _user = response;
+      _errorMessage = null;
+      notifyListeners();
+
+      _user = await userService.getUserData();
     } catch (e) {
       _errorMessage = e.toString();
     } finally {
       _isLoading = false;
+      notifyListeners();
     }
   }
 }
