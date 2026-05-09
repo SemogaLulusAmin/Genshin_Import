a67c5fc887e50b39b67b472d8b9f07810a217ca6
jetasunanda <sunandajeta@gmail.com>
2026-05-09 17:00:17 +0700

    refactor: remove unnecessary string interpolation in URI parsing

--- frontend/lib/services/weapon_service.dart
+++ frontend/lib/services/weapon_service.dart
@@ -17,7 +17,7 @@ class WeaponService {
       }
 
       final response = await http.get(
-        Uri.parse('$baseUrl'),
+        Uri.parse(baseUrl),
         headers: {'Authorization': 'Bearer $token'},
       );
 
