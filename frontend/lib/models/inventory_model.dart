17a9d22aecf2dfe4cf11346764eef688490f8066
jetasunanda <sunandajeta@gmail.com>
2026-06-05 20:14:34 +0700

    refactor: update comment for clarity in split method of inventory_model.dart

--- frontend/lib/models/inventory_model.dart
+++ frontend/lib/models/inventory_model.dart
@@ -21,7 +21,7 @@ abstract class Inventory {
     if (name.isEmpty) return name;
 
     return name
-        .split('-') // Memecah string berdasarkan tanda "-"
+        .split('-') // Split the string by "-"
         .map(
           (word) => word.isNotEmpty
               ? '${word[0].toUpperCase()}${word.substring(1).toLowerCase()}'
